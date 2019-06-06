9793d424752bda8f3a93e596bbcaff63f67bffa3
Nikita <nloetkemann@fh-bielefeld.de>
2019-06-06 13:10:16 +0200

    python kram

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,13 @@
-from src.text_to_voice import text_2_voice
+from src.text_to_voice import text_2_voice, text_2_voice2
 from src.voice_to_text import voice_2_text
 import time
 
 if __name__ == "__main__":
-    text = voice_2_text()
+    # text = voice_2_text()
     #print(text)
     #if text is not None:
     #    text_2_voice(text)
     #print(voice_2_text())
     #time.sleep(2)
-    #text_2_voice("Schön deine Stimme zu hören.")
-    #text_2_voice("Ich liebe dich")
+    # text_2_voice("Schön deine Stimme zu hören.")
+    text_2_voice2("Schön deine Stimme zu hören.")
