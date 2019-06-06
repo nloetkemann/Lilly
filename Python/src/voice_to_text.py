d03f09a374e0404281fa76ad0a0418f4b78a9296
Nikita <nloetkemann@fh-bielefeld.de>
2019-06-06 17:01:14 +0200

    all recognizer

--- Python/src/voice_to_text.py
+++ Python/src/voice_to_text.py
@@ -4,10 +4,30 @@ import speech_recognition as sr
 def voice_2_text():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Speak:")                                                                                   
+        print("Speak:")
         audio = r.listen(source)
         print("Finished!")
     try:
         return r.recognize_bing(audio)
     except Exception as e:
-        return e
+        print(e)
+    try:
+        return r.recognize_google(audio)
+    except Exception as e:
+        print(e)
+    try:
+        return r.recognize_ibm(audio)
+    except Exception as e:
+        print(e)
+    try:
+        return r.recognize_sphinx(audio)
+    except Exception as e:
+        print(e)
+    try:
+        return r.recognize_wit(audio)
+    except Exception as e:
+        print(e)
+    try:
+        return r.recognize_houndify(audio)
+    except Exception as e:
+        print(e)
