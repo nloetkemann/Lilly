cec77a460b7f79c4e1a33e917b154470266bd25e
Nikita <nloetkemann@fh-bielefeld.de>
2019-06-06 22:35:17 +0200

    cli

--- Python/src/voice_to_text.py
+++ Python/src/voice_to_text.py
@@ -9,6 +9,7 @@ API_ENDPOINT = 'https://api.wit.ai/speech'
 # Wit.ai api access token
 wit_access_token = os.environ['WIT_TOKEN']
 
+
 # records from mic and returns what it understood
 def voice_2_text():
     r = sr.Recognizer()
@@ -21,27 +22,28 @@ def voice_2_text():
     except Exception as e:
         print(e)
         return None, None, None
-        
-def voice_2_text2(num_seconds = 5):
+
+
+def voice_2_text2(num_seconds=5):
     audiofile = 'sounds/temp.wav'
     # record audio of specified length in specified audio file
     record_audio(num_seconds, audiofile)
- 
+
     # reading audio
     audio = read_audio(audiofile)
     os.remove(audiofile)
- 
+
     # defining headers for HTTP request
     headers = {'authorization': 'Bearer ' + wit_access_token,
                'Content-Type': 'audio/wav'}
- 
+
     # making an HTTP post request
-    resp = requests.post(API_ENDPOINT, headers = headers,
-                         data = audio)
- 
+    resp = requests.post(API_ENDPOINT, headers=headers,
+                         data=audio)
+
     # converting response content to JSON format
     data = json.loads(resp.content)
- 
+
     # get text from data
     text = data['_text']
     if 'entities' in data:
@@ -50,11 +52,9 @@ def voice_2_text2(num_seconds = 5):
             entity = entities[0]
             values = []
             for value in data['entities'][entity]:
-                 values.append(value['value'])
-                 
-            return text, entity, values
-        
-     # return the text
-    return text, None, None
+                values.append(value['value'])
 
-	
+            return text, entity, values
+
+    # return the text
+    return text, None, None
