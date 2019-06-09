96620242e6f0c23bc09771bb74859c61eb3c5f35
Nikita <nloetkemann@fh-bielefeld.de>
2019-06-09 09:52:11 +0200

    recognize

--- Python/src/wit.py
+++ Python/src/wit.py
@@ -1,5 +1,10 @@
+import json
 import pyaudio
 import wave
+from urllib.request import Request, urlopen
+
+from speech_recognition import AudioData, RequestError, UnknownValueError
+from urllib.error import URLError, HTTPError
 
 
 def record_audio(RECORD_SECONDS, WAVE_OUTPUT_FILENAME):
@@ -58,3 +63,29 @@ def read_audio(WAVE_FILENAME):
     with open(WAVE_FILENAME, 'rb') as f:
         audio = f.read()
     return audio
+
+
+def recognize_wit(audio_data, key, show_all=False):
+    assert isinstance(audio_data, AudioData), "Data must be audio data"
+    assert isinstance(key, str), "``key`` must be a string"
+
+    wav_data = audio_data.get_wav_data(
+        convert_rate=None if audio_data.sample_rate >= 8000 else 8000,  # audio samples must be at least 8 kHz
+        convert_width=2  # audio samples should be 16-bit
+    )
+    url = "https://api.wit.ai/speech?v=20160526"
+    request = Request(url, data=wav_data,
+                      headers={"Authorization": "Bearer {}".format(key), "Content-Type": "audio/wav"})
+    try:
+        response = urlopen(request, timeout=None)
+    except HTTPError as e:
+        raise RequestError("recognition request failed: {}".format(e.reason))
+    except URLError as e:
+        raise RequestError("recognition connection failed: {}".format(e.reason))
+    response_text = response.read().decode("utf-8")
+    result = json.loads(response_text)
+
+    # return results
+    if show_all: return result
+    if "_text" not in result or result["_text"] is None: raise UnknownValueError()
+    return result["_text"]
