0f75ba14c959e3358af938317636b175a32e2449
nloetkemann <nloetkemann@fh-bielefeld.de>
2019-06-09 17:57:42 +0200

    dead code

--- Python/src/voice_to_text.py
+++ Python/src/voice_to_text.py
@@ -4,7 +4,7 @@ import requests
 import json
 import time
 
-from src.wit import record_audio, read_audio
+from src.wit import record_audio, read_audio, recognize_wit
 
 # Wit speech API endpoint
 API_ENDPOINT = 'https://api.wit.ai/speech'
@@ -13,7 +13,7 @@ wit_access_token = os.environ['WIT_TOKEN']
 
 
 # records from mic and returns what it understood
-# recognition from google is 1,4 sec faster
+#
 def voice_2_text():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -22,54 +22,9 @@ def voice_2_text():
         print("Finished")
     try:
         time1 = time.time()
-        value = r.recognize_wit(audio, wit_access_token)
+        value = recognize_wit(audio, wit_access_token)
         print(time.time() - time1)
-        # time1 = time.time()
-        # value = r.recognize_google(audio, language="de")
-        # print(time.time() - time1)
         return value, None, None
     except Exception as e:
         print(e)
         return None, None, None
-
-
-# records by default 5 seconds, sends direct api request
-def voice_2_text2(num_seconds=5):
-    time1 = time.time()
-    audiofile = 'sounds/temp.wav'
-    # record audio of specified length in specified audio file
-    record_audio(num_seconds, audiofile)
-
-    # reading audio
-    audio = read_audio(audiofile)
-    os.remove(audiofile)
-
-    # defining headers for HTTP request
-    headers = {'authorization': 'Bearer ' + wit_access_token,
-               'Content-Type': 'audio/wav'}
-
-    # making an HTTP post request
-    resp = requests.post(API_ENDPOINT, headers=headers,
-                         data=audio)
-
-    # converting response content to JSON format
-    data = json.loads(resp.content)
-
-    # get text from data
-    text = data['_text']
-    if 'entities' in data:
-        entities = list(data['entities'].keys())
-        if len(entities) > 0:
-            entity = entities[0]
-            values = []
-            for value in data['entities'][entity]:
-                values.append(value['value'])
-            time2 = time.time()
-            print(time2 - time1)
-
-            return text, entity, values
-
-    # return the text
-    time2 = time.time()
-    print(time2 - time1)
-    return text, None, None
