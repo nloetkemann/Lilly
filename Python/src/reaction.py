4a2e60a881d72b15485df1c3449789a2abaec165
nloetkemann <nloetkemann@fh-bielefeld.de>
2019-06-19 14:45:05 +0200

    reaction

---
+++ Python/src/reaction.py
@@ -0,0 +1,11 @@
+from random import randint
+
+# chooses von of the values
+def react(values):
+    index = randint(0, len(values))
+    return values[index]
+
+
+# uses the keyword for a search, e.g. the weather
+def search(keyword):
+    pass
