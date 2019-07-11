72d0b102a2f62e80e56c53850216901e622ab49c
Nikita <nloetkemann@fh-bielefeld.de>
2019-07-11 13:34:25 +0200

    changes

--- Server/src/grpc/file_service.py
+++ Server/src/grpc/file_service.py
@@ -1,7 +1,7 @@
 import src.grpc.pb.file_pb2_grpc as file_pb2_grpc
-from src.grpc.pb import file_pb2
+from src.grpc.pb import file_pb2, message_pb2
 import itertools
-
+from src.grpc.message_service import response_queue
 from src.logic.handler import MessageHandler
 from src.wit.wit import send_audio_file
 
@@ -10,15 +10,19 @@ class FileServicer(file_pb2_grpc.FileServicer):
     temp_dir = '../../temp/'
 
     def UploadFile(self, request_iterator, context):
-        def save_chunks_to_file(chunks):
-            name = (list(itertools.islice(chunks, 1))[0]).name
+        def save_chunks_to_file(parts):
+            first_part = (list(itertools.islice(parts, 1))[0])
+            name = first_part.name
             filename = self.temp_dir + name
+            client_type = first_part.client_type
             with open(filename, 'wb') as f:
-                for chunk in chunks:
-                    if not chunk.HasField('name'):
-                        f.write(chunk.buffer)
-            return filename
-        filename = save_chunks_to_file(request_iterator)
+                for single_part in parts:
+                    if not single_part.HasField('name'):
+                        f.write(single_part.buffer)
+            return filename, client_type
+
+        filename, client_type = save_chunks_to_file(request_iterator)
         result = send_audio_file(filename)
         response = MessageHandler(result).handle_message()
-        return file_pb2.FileResponse(text=response.text)
+        response_queue.append((response, client_type))
+        return message_pb2.Success(success=True)
--- Server/src/grpc/message_service.py
+++ Server/src/grpc/message_service.py
@@ -3,9 +3,10 @@ from src.logic.handler import MessageHandler
 from src.wit.wit import send_text
 import src.grpc.pb.message_pb2_grpc as message_pb2_grpc
 
+response_queue = []
+
 
 class MessageServicer(message_pb2_grpc.MessageServicer):
-    response_queue = []
 
     def SingleRequest(self, request, context):
         wit_response = send_text(request.body)
@@ -14,13 +15,13 @@ class MessageServicer(message_pb2_grpc.MessageServicer):
         except Exception as e:
             print(e)
             return message_pb2.Success(success=False)
-        self.response_queue.append((response, request.client_type))
+        response_queue.append((response, request.client_type))
         return message_pb2.Success(success=True)
 
     def StreamRequest(self, request_iterator, context):
         while True:
-            while len(self.response_queue) > 0:
-                response = (self.response_queue[0])[0]
-                client_type = (self.response_queue[0])[1]
-                self.response_queue.pop(0)
+            while len(response_queue) > 0:
+                response = (response_queue[0])[0]
+                client_type = (response_queue[0])[1]
+                response_queue.pop(0)
                 yield message_pb2.MessageResponse(body=response.text, client_type=client_type)
