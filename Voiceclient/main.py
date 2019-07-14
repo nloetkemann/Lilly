d831d74d31c4cd05f4129a0ee2afe3f3a1fcaed0
nloetkemann <nloetkemann@fh-bielefeld.de>
2019-07-14 19:45:00 +0200

    try

--- Voiceclient/main.py
+++ Voiceclient/main.py
@@ -4,8 +4,9 @@ from src.logic.voice_to_text import voice_2_text
 client = Client()
 
 if __name__ == "__main__":
-    # pass
     audio_data = voice_2_text()
+    file = open('./temp/temp.wav', 'wb')
+    file.write(audio_data)
     client.upload_file(audio_data)
     # assert isinstance(wit_response, WitResponse)
     # handler = MessageHandler(wit_response)
--- Voiceclient/src/grpc/client.py
+++ Voiceclient/src/grpc/client.py
@@ -14,21 +14,24 @@ class Client:
     CHUNK_SIZE = 1024 * 512
 
     def __init__(self):
-        channel = grpc.insecure_channel('localhost:50051')
+        channel = grpc.insecure_channel('192.168.178.23:50051')
         self.message_stub = message_pb2_grpc.MessageStub(channel)
         self.file_stub = file_pb2_grpc.FileStub(channel)
         self.thread = FunctionThread(self._wait_for_response)
         self.thread.start()
 
-    def upload_file(self, audio_data):
+    def upload_file(self, filename):
         def chunk_file():
-            client_type = ClientType(telegramm=ClientType.LA())
-            yield FileRequest(name='temp.wav', client_type=client_type)
-            while True:
-                # piece = file.read(self.CHUNK_SIZE)
-                # if len(piece) == 0:
-                #     return
-                yield FileRequest(buffer=audio_data)
+            client_type = ClientType(la=ClientType.LA())
+            yield FileRequest(name=filename, client_type=client_type)
+            print('nachricht gesendet')
+            print(filename)
+            with open(filename, 'rb') as file:
+                while True:
+                    piece = file.read(self.CHUNK_SIZE)
+                    if len(piece) == 0:
+                        return
+                    yield FileRequest(buffer=piece)
 
         result = self.file_stub.UploadFile(chunk_file())
         return result.success
@@ -39,6 +42,7 @@ class Client:
 
     def _wait_for_response(self, stop_thread):  # todo muss noch stopthread einbauen
         responses = self._get_messages_from_server()
+        print('waiting for response')
         for response in responses:
             client_type = response[1]
             if client_type.telegramm is not None and client_type.telegramm != '':
